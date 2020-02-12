775ba1558353c1d77c2be505cb783a7202487e95
Nathaniel Fargo <nathaniel.fargo@gmail.com>
2020-02-11 18:43:13 -0700

    Changed connection way

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
-const env = process.env;
-// Configures it for mongo connection
+// Configures mongoose
 module.exports = {
-    string: `mongodb+srv://${env.DB_USER}:${env.DB_PASS}@cluster0-baim8.gcp.mongodb.net/${env.DB}?retryWrites=true`,
+    string: process.env.MONGO_CONNECTION,
     constructor: {
         useNewUrlParser: true,
         useCreateIndex: true,
